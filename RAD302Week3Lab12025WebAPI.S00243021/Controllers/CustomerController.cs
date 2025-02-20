8878e39036ce8925c494f2bca2c9c1856ffb4056
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2025-02-20 18:14:42 +0000

    Week3 S00243021 Lab 1 Customer Web API Call setup and tested

---
+++ RAD302Week3Lab12025WebAPI.S00243021/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RAD302Week3Lab12025CL.S00243021;
+using Tracker.WebAPIClient;
+
+namespace RAD302Week3Lab12025WebAPI.S00243021.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomer<Customer> _customerRepository;
+
+        public CustomerController(ICustomer<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            ActivityAPIClient.Track(StudentID: "S00243021", StudentName: "Dmytro Severin", activityName: "Rad302 Week 3 Lab 1", Task: "Testing basic controller call");
+
+            return _customerRepository.GetAll();
+        }
+    }
+}
